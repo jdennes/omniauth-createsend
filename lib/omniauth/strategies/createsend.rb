f90e2f26622c69a74397acc23380b06cc52ea506
James Dennes <jdennes@gmail.com>
2015-01-19 14:00:53 +0100

    Use double-quotes everywhere consistently

--- lib/omniauth/strategies/createsend.rb
+++ lib/omniauth/strategies/createsend.rb
@@ -1,16 +1,16 @@
-require 'omniauth/strategies/oauth2'
+require "omniauth/strategies/oauth2"
 
 module OmniAuth
   module Strategies
     class CreateSend < OmniAuth::Strategies::OAuth2
       option :name, "createsend"
       option :client_options, {
-        :site => 'https://api.createsend.com',
-        :authorize_url => '/oauth',
-        :token_url => '/oauth/token'
+        :site => "https://api.createsend.com",
+        :authorize_url => "/oauth",
+        :token_url => "/oauth/token"
       }
     end
   end
 end
 
-OmniAuth.config.add_camelization 'createsend', 'CreateSend'
+OmniAuth.config.add_camelization "createsend", "CreateSend"
